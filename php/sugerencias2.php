620d58a3eebae5e47a655d7cb6c5ef20f35195e8
XxBladexX <ezelorenzog@gmail.com>
2019-05-02 19:38:23 -0400

    Summary

--- php/sugerencias2.php
+++ php/sugerencias2.php
@@ -11,6 +11,9 @@ echo "Correo enviado";
   <link rel="stylesheet" href="CSS/carga.css">
   </head>
   <body>
+  <div class = "preload" id = "preload">
+  <div class = "loader1" id = "loader1"></div>
+  <div class = "loader2" id = "loader2"></div>
   </body>
   </html>
 }
