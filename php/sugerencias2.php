5a4c33c0e023e6a6d9afa85f823ada7b5ea6737d
XxBladexX <ezelorenzog@gmail.com>
2019-05-07 19:51:00 -0400

    Summary

--- php/sugerencias2.php
+++ php/sugerencias2.php
@@ -2,25 +2,7 @@
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-if(mail('pwbooks19@gmail.com' , $asunto, $mensaje)){
-echo "Correo enviado";
-<!DOCTYPE html>
-<html lang ="en">
- <head>
-  <meta charset = "utf-8">
-  <link rel="stylesheet" href="CSS/carga.css">
-  </head>
-  <body>
-  <div class = "preload" id = "preload">
-  <div class = "loader1" id = "loader1"></div>
-  <div class = "loader2" id = "loader2"></div>
-  </body>
-  </html>
-}
-else{
-
-}
-
+mail('pwbooks19@gmail.com' , $asunto, $mensaje);
 
 
 ?>
