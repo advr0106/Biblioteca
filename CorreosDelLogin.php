f48f10127ed4199247f0337f51f5d037f25490f9
XxBladexX <ezelorenzog@gmail.com>
2019-05-12 19:31:49 -0400

    Summary

--- CorreosDelLogin.php
+++ CorreosDelLogin.php
@@ -9,9 +9,9 @@ $asunto = "Contacto desde nuestra web";
 
 // Enviando Mensaje
 if(mail($destinatario, $asunto, $carta)){
-  include 'SugerenciasEnviadas.html';
+  include 'RegistrarseC.html';
 }
 else{
-  include 'SugerenciasEnviadas.html';
+  include 'RegistrarseC.html';
 }
 ?>
