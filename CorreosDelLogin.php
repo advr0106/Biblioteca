8537f534e6b69ce200184fae57d6060c1939d941
XxBladexX <ezelorenzog@gmail.com>
2019-05-12 19:16:36 -0400

    Summary

--- CorreosDelLogin.php
+++ CorreosDelLogin.php
@@ -5,15 +5,9 @@ $mensaje = $_POST['mensaje'];
 $destinatario = "ezelorenzog@gmail.com";
 $asunto = "Contacto desde nuestra web";
 
-$carta .= "Correo: $correo \n";
 $carta .= "Mensaje: $mensaje";
 
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta);
-header('SugerenciasEnviadas.html');
-
-
-
-  mail($destinatario, $asunto, $carta);
-  header('Location:mensaje-de-envio.html');
+include 'SugerenciasEnviadas.html';
 ?>
